ca74f3af3f3f2dd6033a110a01619f6c786a3cfa
chloe2781 <chloe.nguyen.2708@gmail.com>
2024-01-29 19:36:09 -0500

    Initial code

---
+++ nailARt/nailARt/LogInView.swift
@@ -0,0 +1,19 @@
+//
+//  LogInView.swift
+//  nailARt
+//
+//  Created by Chloe Nguyen on 1/29/24.
+//
+
+import Foundation
+import UIKit
+
+class LogInView: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
---
+++ nailARt/nailARt/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  nailARt
+//
+//  Created by Chloe Nguyen on 1/29/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
