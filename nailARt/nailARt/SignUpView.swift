2bc519cd5aa5daaa8108a58be91a44a872368b24
chloe2781 <chloe.nguyen.2708@gmail.com>
2024-02-12 23:12:52 -0500

    Adding sign up

---
+++ nailARt/nailARt/SignUpView.swift
@@ -0,0 +1,19 @@
+//
+//  SignUpView.swift
+//  nailARt
+//
+//  Created by Chloe Nguyen on 2/12/24.
+//
+
+import Foundation
+import UIKit
+
+class SignUpView: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
