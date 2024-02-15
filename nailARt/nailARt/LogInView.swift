748f45be75c12bfaa595186c158f16b78a04934e
chloe2781 <chloe.nguyen.2708@gmail.com>
2024-02-15 16:45:22 -0500

    Added firebase user auth

--- nailARt/nailARt/LogInView.swift
+++ nailARt/nailARt/LogInView.swift
@@ -7,11 +7,27 @@
 
 import Foundation
 import UIKit
+import FirebaseAuth
 
 class LogInView: UIViewController {
 
+    @IBOutlet weak var emailText: UITextField!
+    
+    @IBOutlet weak var passwordText: UITextField!
+    
     @IBAction func logInButton(_ sender: Any) {
-        self.performSegue(withIdentifier: "home", sender: self)
+        let email = emailText.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+        let password = passwordText.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        Auth.auth().signIn(withEmail: email, password: password) { (result, error) in
+            if error != nil {
+                let alert = UIAlertController(title: "Error", message: "Login Failed: Your user ID or password is incorrect", preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "Dismiss", style: .default, handler: nil))
+                self.present(alert, animated: true)
+            } else {
+                self.performSegue(withIdentifier: "home", sender: self)
+            }
+        }
     }
     
     @IBAction func signUpButton(_ sender: Any) {
